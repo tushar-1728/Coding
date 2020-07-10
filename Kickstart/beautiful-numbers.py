bc236f3b526f4966288bd27ae196460190b14849
tushar-1728 <tushar-1728@github.com>
2020-07-10 22:51:12 +0530

    added-on-10-07-20

---
+++ Kickstart/beautiful-numbers.py
@@ -0,0 +1,16 @@
+from math import log
+t = int(input())
+for i in range(1, t+1):
+    n = int(input())
+    arr = []
+    narr = []
+    for i in range(1, n):
+        if n%i == 1:
+            arr.append(i)
+    for i in arr:
+        temp = log(n*(i-1)+1, i)
+        # if temp%1 == 0:
+        #     narr.append(temp)
+        print(i, temp)
+    print(arr)
+    print(narr)
--- Kickstart/diwali-lightings.py
+++ Kickstart/diwali-lightings.py
@@ -1,13 +1,20 @@
 t = int(input())
 for i in range(t):
-    patt = input()
-    s = len(patt)
-    l,r = map(int, input().split())
-    l -= 1
-    lcount = 0
-    rcount = 0
-    for i in range(s):
-        if(patt[i] == "B"):
-            lcount += (l-(i+1))//s + 1
-            rcount += (r-(i+1))//s + 1
-    print(rcount-lcount)
+    pattern = input()
+    lindex, rindex = map(int, input().split())
+    d = len(pattern)
+    a_list = []
+    r_count = 0
+    l_count = 0
+    flag = 0
+    for j in range(d):
+        if pattern[j] == "B":
+            a_list.append(j +1)
+    for j in a_list:
+        temp = (rindex - j)//d + 1
+        r_count += temp
+        temp = (lindex - j)//d + 1
+        l_count += temp
+        if (lindex - j) % d == 0:
+            flag = 1
+    print("Case #", i+1, ": ", r_count - l_count + flag, sep="")
