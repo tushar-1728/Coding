a4fc97ca4b08c57a62c2ea7b7e4f603f9909d7b0
tushar-1728 <tushar-1728@github.com>
2020-07-12 13:59:55 +0530

    added-on-12-07-20

--- Kickstart/beautiful-numbers.py
+++ Kickstart/beautiful-numbers.py
@@ -1,15 +1,28 @@
-from math import log
+from math import log, sqrt
 t = int(input())
 for i in range(1, t+1):
     n = int(input())
-    arr = []
-    narr = []
-    for i in range(1, n):
-        if n%i == 1:
-            arr.append(i)
-    for i in arr:
-        temp = log(n*(i-1)+1, i)
-        # if temp%1 == 0:
-        #     narr.append(temp)
-    print(arr)
-    print(narr)
+    if n == 3:
+        print("Case #", i, ": ", 2, sep="")
+    else:
+        arr1 = []
+        arr2 = []
+        temp_n = int(sqrt(n))
+        flag = 0
+        for j in range(1, temp_n):
+            if (n-1)%j == 0:
+                arr1.append(j)
+                arr2.insert(0, (n-1)//j)
+        if (n-1)%temp_n == 0:
+            arr1.append(temp_n)
+        arr1.pop(0)
+        # print(arr1)
+        # print(arr2)
+        arr = arr1 + arr2
+        # print(arr)
+        for j in arr:
+            temp = log(n*(j-1)+1, j)
+            if temp%1 == 0:
+                res = j
+                break
+        print("Case #", i, ": ", res, sep="")
