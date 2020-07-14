b19e0ec74a85a7b1af6538c9b36c4f8c8fcfc852
tushar-1728 <tushar-1728@github.com>
2020-07-14 23:45:57 +0530

    added-on-14-07-20

--- LeetCode/0066-plus-one.py
+++ LeetCode/0066-plus-one.py
@@ -1,3 +1,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        
+        s = ""
+        res = []
+        if len(digits) != 0:
+            for i in digits:
+                s += str(i)
+            s = int(s) + 1
+            while(s != 0):
+                res.insert(0, s%10)
+                s //= 10
+        return res
